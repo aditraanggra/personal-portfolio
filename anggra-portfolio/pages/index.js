55855a8f368acd47b5ad8f13028b54025381aac9
Anggra Aditra <aditra.anggra@gmail.com>
2022-01-14 13:33:37 +0700

    Install Next.js & Tailwind

---
+++ anggra-portfolio/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='w-full flex justify-center items-center'>
+      <h1 className='font-sans text-2xl font-semibold text-teal-600'>
+        Welcome to NextJS
+      </h1>
+    </div>
+  )
+}
